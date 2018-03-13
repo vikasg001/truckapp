fcbfcf90614c6fbfdd063e864a9f4e2ddea38103
vikasg001 <vikasg001@gmail.com>
2018-03-13 12:13:13 +0530

    Update app.constant.ts

--- src/app/shared/app.constant.ts
+++ src/app/shared/app.constant.ts
@@ -2,7 +2,7 @@
  * Created by npandey on 3/28/2017.
  */
 export class AppConstant {
-  public static API_URL = 'http://metalapp.co.nf/metaladmin/jsonData.php';
+  public static API_URL = ''; // put your service url for database backuo
   public static APP_SERVER_VERSION = '1.0';
   public static APP_VERSION = '1.0.4';
   public static APP_VERSION_TYPE = '(alpha)';
